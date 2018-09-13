032eacc6943765b8398f057c2c85ac9e68ac6441
Nopakorn Ganjanasinit <nopakorn.g@ssd.co.th>
2018-09-13 22:08:27 +0700

    initial project

---
+++ geometric-demo/geometric-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  geometric-demo
+//
+//  Created by Nopakorn Ganjanasinit on 13/9/2561 BE.
+//  Copyright © 2561 Nopakorn Ganjanasinit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
